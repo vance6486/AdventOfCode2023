afd3b1b801ea2d441bc1e668f10496a2af453d04
vance6486 <105457971+vance6486@users.noreply.github.com>
2023-12-13 16:38:52 -0600

    Added starter for day 11

---
+++ AdventOfCode11/AdventOfCode11/Galaxy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode11
+{
+    internal class Galaxy
+    {
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+        public int Id { get; private set; }
+
+        //public List<GalaxyPair> AllGalaxyPairs = new List<GalaxyPair>();
+
+        public Galaxy PairedGalaxy = null;
+        public bool HasPairedGalaxy {  get; private set; }
+
+
+        public Galaxy()
+        {
+            Row = 0;
+            Col = 0;
+        }
+
+        public Galaxy(int row, int col)
+        {
+            Row = row;
+            Col = col;
+
+            Id = (row*1000) + col;
+        }
+
+        //public void AddPair(Galaxy otherGalaxy, int distance)
+        //{
+        //    if(this != otherGalaxy)
+        //    {
+        //        AllGalaxyPairs.Add(new GalaxyPair(this, otherGalaxy, distance));
+        //        AllGalaxyPairs.Sort();
+        //    }
+                
+        //}
+
+        //public bool hasGalaxyPair(Galaxy otherGalaxy)
+        //{
+        //    bool hasPair = false;
+
+        //    foreach (GalaxyPair pair in AllGalaxyPairs)
+        //    {
+        //        if(pair.GalaxyB == otherGalaxy)
+        //        {
+        //            hasPair = true;
+        //        }
+        //    }
+
+        //    return hasPair;
+        //}
+
+        public override bool Equals(object obj)
+        {
+            return Id.Equals(((Galaxy)obj).Id);
+        }
+
+    }
+}
---
+++ AdventOfCode11/AdventOfCode11/Space.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode11
+{
+    internal class Space
+    {
+        public bool IsEmpty { get; private set; } = true;
+        public Galaxy GalaxyContents { get; private set; } = null;
+
+        public int Row;
+        public int Col;
+
+        public bool Visited = false;
+        public int Steps = 0;
+
+        public Space() { }
+
+        public Space(char contentsChar, int row, int col)
+        {
+            Row = row; 
+            Col = col;
+
+            if (contentsChar == '#' )
+            {
+                IsEmpty = false;
+                GalaxyContents = new Galaxy(row, col);
+            }
+        }
+
+        public void ResetVisited() { Visited = false; }
+    }
+}
